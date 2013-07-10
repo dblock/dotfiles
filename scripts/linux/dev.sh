f5a08e348c0a4ce28d8c4f48c53f70ef02c8f678
dB <dblock@dblock.org>
2013-07-09 22:04:05 -0400

    Added libgsl-dev, Google Scientific Library.

--- scripts/linux/dev.sh
+++ scripts/linux/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install build-essential bison openssl libreadline5 libreadline-dev curl git-core zlib1g zlib1g-dev libssl-dev libreadline-dev libxml2-dev autoconf libxslt-dev
+sudo apt-get install build-essential bison openssl libreadline5 libreadline-dev curl git-core zlib1g zlib1g-dev libssl-dev libreadline-dev libxml2-dev autoconf libxslt-dev libgsl0-dev
