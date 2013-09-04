d0ff8ca779210f1acbbdc9b02d6d03f99241b54b
dblock <dblock@dblock.org>
2013-09-04 12:04:52 -0400

    Install 1.9.3 and 2.0 rubies.

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+set -e
+
 bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+
+rvm autolibs enable
+
+rvm install 1.9.3
+rvm install 2.0
+
+rvm use 2.0 --default
