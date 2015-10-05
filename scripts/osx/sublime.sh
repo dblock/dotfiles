56f329a734eb5149c2e77c47aaa69203eb23e8b9
dblock <dblock@dblock.org>
2015-10-05 08:45:44 -0400

    Added sublime text symlinks.

---
+++ scripts/osx/sublime.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/sublime
