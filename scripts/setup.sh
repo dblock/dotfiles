f4730360362aefc85976e7e03cf79d8426301848
dblock <dblock@dblock.org>
2017-01-06 17:36:06 -0500

    Added JSBeautify for Sublime Text.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,7 @@ echo "Setting up from $DOTFILES_DIR ..."
 [[ ! -s "$HOME/.git-prompt.sh" ]] && ln -s "$DOTFILES_DIR/bash/.git-prompt.sh" "$HOME/.git-prompt.sh"
 [[ ! -s "$HOME/.git-completion.sh" ]] && ln -s "$DOTFILES_DIR/bash/.git-completion.sh" "$HOME/.git-completion.sh"
 [[ ! -s "$HOME/.bin" ]] && ln -s "$DOTFILES_DIR/bash/bin" "$HOME/.bin"
+[[ ! -s "$HOME/.jsbeautifyrc" ]] && ln -s "$DOTFILES_DIR/bash/.jsbeautifyrc" "$HOME/.jsbeautifyrc"
 
 if ! grep -q "$DOTFILES_DIR/bash/.bashrc" "$HOME/.bashrc"; then
   echo "Appending to $HOME/.bashrc ..."
