56409ca4ab3b2bca6ac8bc50752c1dfc47605d59
dblock <dblock@dblock.org>
2019-05-02 21:28:34 -0400

    Location of hosts moved to HTTPs.

--- scripts/hosts.sh
+++ scripts/hosts.sh
@@ -4,5 +4,5 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 [[ ! -e "$DOTFILES_DIR/backup" ]] && mkdir "$DOTFILES_DIR/backup"
 sudo cp /etc/hosts "$DOTFILES_DIR/backup/hosts.backup"
 
-sudo curl --silent http://someonewhocares.org/hosts/hosts > /etc/hosts
+sudo curl --silent https://someonewhocares.org/hosts/hosts > /etc/hosts
 
