bc6de00ff6a14e13fd6c0118c1a1d75ba6e2e2df
dblock <dblock@dblock.org>
2019-05-09 14:36:23 -0400

    Force install imagemagick 6.

--- scripts/osx/imagemagick.sh
+++ scripts/osx/imagemagick.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# the *--disable-openmp* option is to be sure to disable OpenMP for ImageMagick
-# it will segfault if ImageMagick is built with it enabled
-# see https://github.com/rmagick/rmagick/issues/16
-
-brew install -f imagemagick --disable-openmp
+brew install imagemagick@6
 
 convert --version
