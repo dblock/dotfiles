b262cbfb1725e5484303d8c187c71688a1f4dddc
dblock <dblock@dblock.org>
2021-08-24 17:00:44 +0000

    Correct import command.
    Signed-off-by: dblock <dblock@dblock.org>

--- scripts/linux/gpg.sh
+++ scripts/linux/gpg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gpg --import-secret-keys ~/Dropbox/Personal/7C94E183.gpg
+gpg --import ~/Dropbox/Personal/7C94E183.gpg
 gpg --import-ownertrust < ~/Dropbox/Personal/7C94E183.trustlevel.txt
 
 git config --global user.signingkey 75BF031B7C94E183
