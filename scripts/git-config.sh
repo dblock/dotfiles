cca88e7cf5e5d7713646bcc23f43ae3b0cc0b930
dblock <dblock@dblock.org>
2021-08-24 16:58:20 +0000

    Use vi for git ammend.

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -12,3 +12,5 @@ git config --global alias.commend 'commit --amend --no-edit'
 git config --global alias.it '!git init && git commit -m "Initial commit." --allow-empty'
 git config --global alias.s 'status --short --branch'
 git config --global alias.grog 'log --graph --abbrev-commit --decorate --all --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)"'
+
+git config --global core.editor 'vi'
